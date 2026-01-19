8ba74ee8e0c5b150359f6f707684f2cc3255e94a
Sathyasri-2056 <sathyasribalraj2056@gmail.com>
2026-01-19 15:58:53 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class demo{
-  public static vaild main(String[]args){
+  public static void main(String[]args){
     Scanner sc= new Scanner(System.in);
     int Attendance= sc.nextInt();
     if(Attendance > 85){
